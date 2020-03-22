598e4982fb5c5f5943a17dd5096ad49f282b0046
Apoorva Mishra <apoorvashukla88@gmail.com>
2020-03-21 23:18:44 -0400

    final commit

---
+++ data/log_parser.py
@@ -0,0 +1,9 @@
+import re
+#from datetime import datetime
+
+file = open('rsvp_agent_log.dat')
+
+for line in file:
+    line = line.rstrip()
+    if re.search('WARNING:', line):
+        print(line[0:14]+' -- '+ line[45:])

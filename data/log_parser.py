01263d0535386aac93d6255f2ea887d989262ce0
Apoorva Mishra <apoorvashukla88@gmail.com>
2020-03-24 20:07:39 -0400

    with regex

--- data/log_parser.py
+++ data/log_parser.py
@@ -6,18 +6,5 @@ with open('rsvp_agent_log.dat', 'r') as file:
     for line in file:
         line = line.rstrip()
         if re.search('WARNING:', line):
-            pattern = re.compile(r'WARNING:.....mailslot_create:')
-            new_line = re.sub(pattern, '--', line)
-            print(new_line)
-
-
-
-
-#             print(line)
-#
-# pattern = re.compile(r'')
-#
-# # pattern = re.compile(r'....')
-# # matches = pattern.finditer()
-# # for line in matches:
-# #     print(line)
+            #print(line)
+            print(re.sub(r'WARNING:\S+:', '--', line))
